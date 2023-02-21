e011376160bae64271511612e415851c8a286893
ZuhebAkhter <zuheb1998@gmail.com>
2023-02-21 21:58:55 +0530

    task 5

--- dom.js
+++ dom.js
@@ -1,12 +1,26 @@
-var headertitle=document.getElementById('header-title');
-headertitle.style.border ='solid 3px #000';
-var titles=document.getElementById('title1');
-titles.style.fontFamily='bold';
-titles.style.color='green';
+// var headertitle=document.getElementById('header-title');
+// headertitle.style.border ='solid 3px #000';
+// var titles=document.getElementById('title1');
+// titles.style.fontFamily='bold';
+// titles.style.color='green';
+
+// var items=document.getElementsByClassName('list-group-item');
+// items[2].style.backgroundColor='Green';
+// items[0].style.fontFamily='bold';
+// items[1].style.fontFamily='bold';
+// items[2].style.fontFamily='bold';
+// items[3].style.fontFamily='bold';
+
+
+var li=document.getElementsByTagName('li');
+for(i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor='red';
+}
+
 
 var items=document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor='Green';
-items[0].style.fontFamily='bold';
-items[1].style.fontFamily='bold';
-items[2].style.fontFamily='bold';
-items[3].style.fontFamily='bold';
+for(i=0;i<items.length;i++)
+{
+    items[i].style.backgroundColor='red';
+}
