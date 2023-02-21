4221c4778991ec39554781a24b0253efc3a259b0
ZuhebAkhter <zuheb1998@gmail.com>
2023-02-21 21:33:18 +0530

    4th task

---
+++ dom1.js
@@ -0,0 +1,6 @@
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='Green';
+items[0].style.fontFamily='bold';
+items[1].style.fontFamily='bold';
+items[2].style.fontFamily='bold';
+items[3].style.fontFamily='bold';
