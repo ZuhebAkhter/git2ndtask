f2acf2dcac5e3591ef2db731293fb176be24d273
ZuhebAkhter <zuheb1998@gmail.com>
2023-02-21 19:52:59 +0530

    donewith3rd

---
+++ dom.js
@@ -0,0 +1,5 @@
+var headertitle=document.getElementById('header-title');
+headertitle.style.border ='solid 3px #000';
+var titles=document.getElementById('title1');
+titles.style.fontFamily='bold';
+titles.style.color='green';
