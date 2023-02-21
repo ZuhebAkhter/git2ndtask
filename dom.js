50f1ee87b45eb11ea552dd23beb45bf1f50f8b6e
ZuhebAkhter <zuheb1998@gmail.com>
2023-02-21 21:29:21 +0530

    task4

--- dom.js
+++ dom.js
@@ -3,3 +3,10 @@ headertitle.style.border ='solid 3px #000';
 var titles=document.getElementById('title1');
 titles.style.fontFamily='bold';
 titles.style.color='green';
+
+var items=document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor='Green';
+items[0].style.fontFamily='bold';
+items[1].style.fontFamily='bold';
+items[2].style.fontFamily='bold';
+items[3].style.fontFamily='bold';
