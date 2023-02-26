cecc5e03c097e536295c82bd36e4d2c2234261b1
ZuhebAkhter <zuheb1998@gmail.com>
2023-02-26 15:05:07 +0530

    9th task

--- dom.js
+++ dom.js
@@ -111,6 +111,8 @@ function addItem(e){
 
   // Get input value
   var newItem = document.getElementById('item').value;
+  var description = document.getElementById('description').value;
+
 
   // Create new li element
   var li = document.createElement('li');
@@ -118,15 +120,20 @@ function addItem(e){
   li.className = 'list-group-item';
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
+  li.appendChild(document.createTextNode(description));
+
 
   // Create del button element
   var deleteBtn = document.createElement('button');
 
+
   // Add classes to del button
   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
 
+
   // Append text node
   deleteBtn.appendChild(document.createTextNode('X'));
+
 
   // Append button to li
   li.appendChild(deleteBtn);
@@ -144,3 +151,18 @@ function removeItem(e){
     }
   }
 }
+function filterItems(e){
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get list
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+  }
